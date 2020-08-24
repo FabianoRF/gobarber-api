0e0579926bdde8fa8b553d2b6af96e6706974686
FabianoRF <freis884@gmail.com>
2020-08-24 15:49:49 -0300

    Project created

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,11 @@
+import { Router} from 'express';
+
+
+const appointmentsRouter = Router();
+
+
+appointmentsRouter.post('/', (request, response)=>{
+  return response.json({ message: 'Hello World' });
+})
+
+export default appointmentsRouter;
