0030d11261a774b6b9cef72292ecc176a3316b97
FabianoRF <freis884@gmail.com>
2020-08-26 21:59:34 -0300

    typeorm config and appointment model and migration created

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import express, { request, response } from 'express';
-
 import routes from './routes';
 
+import './database';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, ()=>{
-  console.log('Server start on port 3333!')
+app.listen(3333, () => {
+  console.log('Server start on port 3333!');
 });
