224464144dcedc871e40ba92e5a4f99edc59d972
FabianoRF <freis884@gmail.com>
2020-08-27 17:04:38 -0300

    route users created

--- src/database/migrations/1598486987022-CreateAppointments.ts
+++ src/database/migrations/1598486987022-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class createAppointments1598486987022
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
@@ -23,6 +23,16 @@ export default class createAppointments1598486987022
             name: 'date',
             type: 'timestamp with time zone',
             isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
           },
         ],
       }),
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -16,14 +16,14 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
     const createApointmentService = new CreateAppointmentService();
 
     const appointment = await createApointmentService.execute({
-      provider,
+      provider_id,
       date: parsedDate,
     });
 
